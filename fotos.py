b6c2acda9c4944eaa4c7a0116b874f51fa402e7a
FateDGG <anghelandres12@gmail.com>
2024-05-28 18:04:06 -0500

    Another try

--- fotos.py
+++ fotos.py
@@ -110,25 +110,4 @@ else:
     image = Image.open(img_file_buffer)
     st.image(image, use_column_width=True)
     
-    # Inicializa una lista vacía para almacenar los nombres de los estudiantes reconocidos
-    student_list = []
-
-    # Realizar la predicción
-    class_name, score = import_and_predict(image, model, class_names)
-
-    # Verificar si el nombre ya está en la lista
-    if class_name not in student_list:
-        student_list.append(class_name)  # Agregar el nombre a la lista de estudiantes reconocidos
-        # Mostrar el resultado
-        if np.max(score) > 0.5:
-            st.subheader(f"Estudiante Identificado: {class_name}")
-            st.text(f"Puntuación de confianza: {100 * np.max(score):.2f}%")
-        else:
-            st.text("No se pudo identificar al estudiante")
-    else:
-        st.text(f"El estudiante {class_name} ya ha sido reconocido")
-
-    # Mostrar el listado de estudiantes
-    st.title("Listado de estudiantes")
-    for student in student_list:
-        st.write(student)
+    # Inicializa una lista vacía para almacenar
